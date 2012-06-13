16d7aaa7a9bf4386ae442436d34a36e089bc4351
unknown <ta.sato@32-23315.OPT-SYS.local>
2012-06-13 14:27:23 +0900

    FIRST COMMIT SATO

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CRNVT1::Application.config.secret_token = '271473a9925e7d381af59f97652e5929235424501175ce6427ac55fac5ce8fc3494a8cad40c9c1c1ddf1e755618f3db56e265f21c1463a5a4aaa814118ac6050'
